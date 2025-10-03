d5abcd603f9171327d34e41fc6263e0cafd2d815
ZacharyAdams <zjadams@ucsc.edu>
2025-10-03 12:47:21 -0700

    completed source for cj1_prompt4.py

--- CodeJournal1/cj1_prompt4.py
+++ CodeJournal1/cj1_prompt4.py
@@ -1,15 +1,5 @@
-# Write a Python program that declares a class describing your favorite animal.
-# Have the data members of the class represent the following physical parameters
-# of the animal:
-#    - length of the arms (float),
-#    - length of the legs (float),
-#    - number of eyes (int),
-#    - does it have a tail? (bool),
-#    - is it furry? (bool).
-# Write an initialization function that sets the values of the data
-# members when an instance of the class is created.
-# Write a member function of the class to print out and describe
-# the data members representing the physical characteristics of the animal.
+# This program defines an Animal class that stores descriptive data about an animal
+# and provides a method to print a human-description to the terminal.
 
 class Animal():
     def __init__(self,
